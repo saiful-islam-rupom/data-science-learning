205df44b5db60ab88b66e3edd8e2201df9dc26b1
Saiful Islam Rupom <saifulislam558855@gmail.com>
2025-04-23 10:46:04 +0600

    Delete

--- python-for-data-science/numpy-a-to-z/numpy-advance/13-working-with-missing-values.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-########### working with missing values ##############
-# a = np.array([1,2,3,4,np.nan,6]) # np.nan - set nan value
-
-# print(a)
-# print(np.isnan(a))    # np.isnan()- if nan, returns true
-# print(a[~(np.isnan(a))])    #  removing missing values by filtering
